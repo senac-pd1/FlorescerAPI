02de00f3ad228275088eebc06a9828acac90c8a2
Wagner <87399006+wagnersouzadepaula@users.noreply.github.com>
2023-05-16 16:57:51 -0300

    Api de usuários (post e login) e plantas (Get e GetId)

---
+++ Migrations/20230514181754_Initial.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlorescerAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Plantas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Categorie = table.Column<string>(type: "varchar", nullable: true),
+                    Desease = table.Column<string>(type: "varchar", nullable: true),
+                    Img = table.Column<string>(type: "varchar", nullable: true),
+                    Use = table.Column<string>(type: "varchar", nullable: false),
+                    LatinName = table.Column<string>(type: "varchar", nullable: true),
+                    Insect = table.Column<string>(type: "varchar", nullable: false),
+                    Avaibility = table.Column<string>(type: "varchar", nullable: true),
+                    Style = table.Column<string>(type: "varchar", nullable: true),
+                    Bearing = table.Column<string>(type: "varchar", nullable: true),
+                    LightTolered = table.Column<string>(type: "varchar", nullable: true),
+                    HeightAtPurchase = table.Column<string>(type: "varchar", nullable: false),
+                    LightIdeal = table.Column<string>(type: "varchar", nullable: true),
+                    WidthAtPurchase = table.Column<string>(type: "varchar", nullable: false),
+                    Appeal = table.Column<string>(type: "varchar", nullable: true),
+                    Perfume = table.Column<string>(type: "varchar", nullable: true),
+                    Growth = table.Column<string>(type: "varchar", nullable: true),
+                    WidthPotential = table.Column<string>(type: "varchar", nullable: false),
+                    Name = table.Column<string>(type: "varchar", nullable: false),
+                    Pruning = table.Column<string>(type: "varchar", nullable: true),
+                    Family = table.Column<string>(type: "varchar", nullable: true),
+                    HeightPotential = table.Column<string>(type: "varchar", nullable: false),
+                    Origin = table.Column<string>(type: "varchar", nullable: true),
+                    Description = table.Column<string>(type: "varchar", nullable: true),
+                    TemperatureMax = table.Column<string>(type: "varchar", nullable: false),
+                    BloomingSeason = table.Column<string>(type: "varchar", nullable: true),
+                    Url = table.Column<string>(type: "varchar", nullable: true),
+                    ColorOfLeaf = table.Column<string>(type: "varchar", nullable: false),
+                    Watering = table.Column<string>(type: "varchar", nullable: true),
+                    ColorOfBloom = table.Column<string>(type: "varchar", nullable: true),
+                    Zone = table.Column<string>(type: "varchar", nullable: false),
+                    AvaibleSizesPot = table.Column<string>(type: "varchar", nullable: true),
+                    OtherName = table.Column<string>(type: "varchar", nullable: true),
+                    TemperatureMin = table.Column<string>(type: "varchar", nullable: false),
+                    PotDiameter = table.Column<string>(type: "varchar", nullable: false),
+                    Climat = table.Column<string>(type: "varchar", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Plantas", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Plantas");
+        }
+    }
+}
---
+++ Models/Planta.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations.Operations;
+
+namespace FlorescerAPI.Models
+{
+    public class Planta
+    {
+        public Guid Id { get; set; }
+        public string? Categorie { get; set; }
+        public string? Desease { get; set; }
+        public string? Img { get; set; }
+        public ICollection<String> Use { get; set; }
+        public string? LatinName { get; set; }
+        public ICollection<String> Insect { get; set; }
+        public string? Avaibility { get; set; }
+        public string? Style { get; set; }
+        public string? Bearing { get; set; }
+        public string? LightTolered { get; set; }
+        public Dictionary<string, double> HeightAtPurchase { get; set; }
+        public string? LightIdeal { get; set; }
+        public Dictionary<string, double> WidthAtPurchase { get; set; }
+        public string? Appeal { get; set; }
+        public string? Perfume { get; set; }
+        public string? Growth { get; set; }
+        public Dictionary<string, double> WidthPotential { get; set; }
+        public string? Name { get; set; }
+        public string? Pruning { get; set; }
+        public string? Family { get; set; }
+        public Dictionary<string, double> HeightPotential { get; set; }
+        public ICollection<String>? Origin { get; set; }
+        public string? Description { get; set; }
+        public Dictionary<string, double> TemperatureMax { get; set; }
+        public string? BloomingSeason { get; set; }
+        public string? Url { get; set; }
+        public ICollection<String> ColorOfLeaf { get; set; }
+        public string? Watering { get; set; }
+        public string? ColorOfBloom { get; set; }
+        public ICollection<String> Zone { get; set; }
+        public string? AvaibleSizesPot { get; set; }
+        public string? OtherName { get; set; }
+        public Dictionary<string, double> TemperatureMin { get; set; }
+        public Dictionary<string, double> PotDiameter { get; set; }
+        public string? Climat { get; set; }
+
+
+
+
+    }
+
+}
