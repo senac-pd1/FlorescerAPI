dd16b62b11e7a3ecd551a5e5ca5d8b4c43a94b9a
Wagner <87399006+wagnersouzadepaula@users.noreply.github.com>
2023-05-31 20:15:18 -0300

    Atualização da tabela de plantas conforme json do matheus.

--- Data/MinimalContextDb.cs
+++ Data/MinimalContextDb.cs
@@ -23,184 +23,26 @@ namespace FlorescerAPI.Data
                 .HasColumnType("varchar");
 
             modelBuilder.Entity<Planta>()
-                .Property(p => p.Categorie)
-                .HasColumnType("varchar");
-
-            modelBuilder.Entity<Planta>()
-                .Property(p => p.Desease)
-                .HasColumnType("varchar");
-
-            modelBuilder.Entity<Planta>()
                 .Property(p => p.Img)
                 .HasColumnType("varchar");
 
-            modelBuilder.Entity<Planta>()
-                .Property(p => p.Use)
-                .HasConversion(
-                    v=> string.Join(',', v), 
-                    v=> v.Split(',', StringSplitOptions.RemoveEmptyEntries)
-                )
-                .HasColumnType("varchar");
-
-            modelBuilder.Entity<Planta>()
-                .Property(p => p.LatinName)
-                .HasColumnType("varchar");
-
-            modelBuilder.Entity<Planta>()
-                .Property(p => p.Insect)
-                .HasConversion(
-                    v => string.Join(',', v),
-                    v => v.Split(',', StringSplitOptions.RemoveEmptyEntries)
-                )
-                .HasColumnType("varchar");
-
-            modelBuilder.Entity<Planta>()
-                .Property(p => p.Avaibility)
-                .HasColumnType("varchar");
-
-            modelBuilder.Entity<Planta>()
-                .Property(p => p.Style)
-                .HasColumnType("varchar");
-
-            modelBuilder.Entity<Planta>()
-                .Property(p => p.Bearing)
-                .HasColumnType("varchar");
-
-            modelBuilder.Entity<Planta>()
-                .Property(p => p.LightTolered)
-                .HasColumnType("varchar");
-
-            modelBuilder.Entity<Planta>()
-                .Property(p => p.HeightAtPurchase)
-                .HasConversion(
-                v => JsonConvert.SerializeObject(v),
-                v => JsonConvert.DeserializeObject<Dictionary<string, double>>(v))
-                .HasColumnType("varchar");
-
-            modelBuilder.Entity<Planta>()
-                .Property(p => p.LightIdeal)
-                .HasColumnType("varchar");
-
-            modelBuilder.Entity<Planta>()
-                .Property(p => p.WidthAtPurchase)
-                .HasConversion(
-                v => JsonConvert.SerializeObject(v),
-                v => JsonConvert.DeserializeObject<Dictionary<string, double>>(v))
-                .HasColumnType("varchar");
-
-            modelBuilder.Entity<Planta>()
-                .Property(p => p.Appeal)
-                .HasColumnType("varchar");
-
-            modelBuilder.Entity<Planta>()
-                .Property(p => p.Perfume)
-                .HasColumnType("varchar");
 
             modelBuilder.Entity<Planta>()
                 .Property(p => p.Growth)
                 .HasColumnType("varchar");
 
             modelBuilder.Entity<Planta>()
-                .Property(p => p.WidthPotential)
-                .HasConversion(
-                v => JsonConvert.SerializeObject(v),
-                v => JsonConvert.DeserializeObject<Dictionary<string, double>>(v))
-                .HasColumnType("varchar");
-
-            modelBuilder.Entity<Planta>()
-                .Property(p => p.Name)
-                .HasColumnType("varchar");
-
-            modelBuilder.Entity<Planta>()
-                .Property(p => p.Pruning)
-                .HasColumnType("varchar");
-
-            modelBuilder.Entity<Planta>()
                 .Property(p => p.Family)
                 .HasColumnType("varchar");
 
-            modelBuilder.Entity<Planta>()
-                .Property(p => p.HeightPotential)
-                .HasConversion(
-                v => JsonConvert.SerializeObject(v),
-                v => JsonConvert.DeserializeObject<Dictionary<string, double>>(v))
-                .HasColumnType("varchar");
 
             modelBuilder.Entity<Planta>()
-                .Property(p => p.Origin)
-                .HasConversion(
-                    v => string.Join(',', v),
-                    v => v.Split(',', StringSplitOptions.RemoveEmptyEntries)
-                )
+                .Property(p => p.Irrigation)
                 .HasColumnType("varchar");
 
-            modelBuilder.Entity<Planta>()
-                .Property(p => p.Description)
-                .HasColumnType("varchar");
 
             modelBuilder.Entity<Planta>()
-                .Property(p => p.TemperatureMax)
-                .HasConversion(
-                v => JsonConvert.SerializeObject(v),
-                v => JsonConvert.DeserializeObject<Dictionary<string, double>>(v))
-                .HasColumnType("varchar");
-
-            modelBuilder.Entity<Planta>()
-                .Property(p => p.BloomingSeason)
-                .HasColumnType("varchar");
-
-            modelBuilder.Entity<Planta>()
-                .Property(p => p.Url)
-                .HasColumnType("varchar");
-
-            modelBuilder.Entity<Planta>()
-                .Property(p => p.ColorOfLeaf)
-                .HasConversion(
-                    v => string.Join(',', v),
-                    v => v.Split(',', StringSplitOptions.RemoveEmptyEntries)
-                )
-                .HasColumnType("varchar");
-
-            modelBuilder.Entity<Planta>()
-                .Property(p => p.Watering)
-                .HasColumnType("varchar");
-
-            modelBuilder.Entity<Planta>()
-                .Property(p => p.ColorOfBloom)
-                .HasColumnType("varchar");
-
-            modelBuilder.Entity<Planta>()
-                .Property(p => p.Zone)
-                .HasConversion(
-                    v => string.Join(',', v),
-                    v => v.Split(',', StringSplitOptions.RemoveEmptyEntries)
-                )
-                .HasColumnType("varchar");
-
-            modelBuilder.Entity<Planta>()
-                .Property(p => p.AvaibleSizesPot)
-                .HasColumnType("varchar");
-
-            modelBuilder.Entity<Planta>()
-                .Property(p => p.OtherName)
-                .HasColumnType("varchar");
-
-            modelBuilder.Entity<Planta>()
-                .Property(p => p.TemperatureMin)
-                .HasConversion(
-                v => JsonConvert.SerializeObject(v),
-                v => JsonConvert.DeserializeObject<Dictionary<string, double>>(v))
-                .HasColumnType("varchar");
-
-            modelBuilder.Entity<Planta>()
-                .Property(p => p.PotDiameter)
-                .HasConversion(
-                v => JsonConvert.SerializeObject(v),
-                v => JsonConvert.DeserializeObject<Dictionary<string, double>>(v))
-                .HasColumnType("varchar");
-
-            modelBuilder.Entity<Planta>()
-                .Property(p => p.Climat)
+                .Property(p => p.Climate)
                 .HasColumnType("varchar");
 
             modelBuilder.Entity<Planta>()
---
+++ Migrations/20230531230959_AtualizaBanco.cs
@@ -0,0 +1,402 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlorescerAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AtualizaBanco : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Appeal",
+                table: "Plantas");
+
+            migrationBuilder.DropColumn(
+                name: "Avaibility",
+                table: "Plantas");
+
+            migrationBuilder.DropColumn(
+                name: "AvaibleSizesPot",
+                table: "Plantas");
+
+            migrationBuilder.DropColumn(
+                name: "Bearing",
+                table: "Plantas");
+
+            migrationBuilder.DropColumn(
+                name: "BloomingSeason",
+                table: "Plantas");
+
+            migrationBuilder.DropColumn(
+                name: "Categorie",
+                table: "Plantas");
+
+            migrationBuilder.DropColumn(
+                name: "Climat",
+                table: "Plantas");
+
+            migrationBuilder.DropColumn(
+                name: "ColorOfBloom",
+                table: "Plantas");
+
+            migrationBuilder.DropColumn(
+                name: "ColorOfLeaf",
+                table: "Plantas");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Plantas");
+
+            migrationBuilder.DropColumn(
+                name: "Desease",
+                table: "Plantas");
+
+            migrationBuilder.DropColumn(
+                name: "HeightAtPurchase",
+                table: "Plantas");
+
+            migrationBuilder.DropColumn(
+                name: "HeightPotential",
+                table: "Plantas");
+
+            migrationBuilder.DropColumn(
+                name: "Insect",
+                table: "Plantas");
+
+            migrationBuilder.DropColumn(
+                name: "LatinName",
+                table: "Plantas");
+
+            migrationBuilder.DropColumn(
+                name: "LightIdeal",
+                table: "Plantas");
+
+            migrationBuilder.DropColumn(
+                name: "LightTolered",
+                table: "Plantas");
+
+            migrationBuilder.DropColumn(
+                name: "Origin",
+                table: "Plantas");
+
+            migrationBuilder.DropColumn(
+                name: "OtherName",
+                table: "Plantas");
+
+            migrationBuilder.DropColumn(
+                name: "Perfume",
+                table: "Plantas");
+
+            migrationBuilder.DropColumn(
+                name: "PotDiameter",
+                table: "Plantas");
+
+            migrationBuilder.DropColumn(
+                name: "Pruning",
+                table: "Plantas");
+
+            migrationBuilder.DropColumn(
+                name: "Style",
+                table: "Plantas");
+
+            migrationBuilder.DropColumn(
+                name: "TemperatureMax",
+                table: "Plantas");
+
+            migrationBuilder.DropColumn(
+                name: "TemperatureMin",
+                table: "Plantas");
+
+            migrationBuilder.DropColumn(
+                name: "Url",
+                table: "Plantas");
+
+            migrationBuilder.DropColumn(
+                name: "Use",
+                table: "Plantas");
+
+            migrationBuilder.DropColumn(
+                name: "Watering",
+                table: "Plantas");
+
+            migrationBuilder.DropColumn(
+                name: "WidthAtPurchase",
+                table: "Plantas");
+
+            migrationBuilder.RenameColumn(
+                name: "Zone",
+                table: "Plantas",
+                newName: "Irrigation");
+
+            migrationBuilder.RenameColumn(
+                name: "WidthPotential",
+                table: "Plantas",
+                newName: "Climate");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Img",
+                table: "Plantas",
+                type: "varchar",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Growth",
+                table: "Plantas",
+                type: "varchar",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Family",
+                table: "Plantas",
+                type: "varchar",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Luminosity",
+                table: "Plantas",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Luminosity",
+                table: "Plantas");
+
+            migrationBuilder.RenameColumn(
+                name: "Irrigation",
+                table: "Plantas",
+                newName: "Zone");
+
+            migrationBuilder.RenameColumn(
+                name: "Climate",
+                table: "Plantas",
+                newName: "WidthPotential");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Img",
+                table: "Plantas",
+                type: "varchar",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Growth",
+                table: "Plantas",
+                type: "varchar",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Family",
+                table: "Plantas",
+                type: "varchar",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Appeal",
+                table: "Plantas",
+                type: "varchar",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Avaibility",
+                table: "Plantas",
+                type: "varchar",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AvaibleSizesPot",
+                table: "Plantas",
+                type: "varchar",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Bearing",
+                table: "Plantas",
+                type: "varchar",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BloomingSeason",
+                table: "Plantas",
+                type: "varchar",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Categorie",
+                table: "Plantas",
+                type: "varchar",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Climat",
+                table: "Plantas",
+                type: "varchar",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ColorOfBloom",
+                table: "Plantas",
+                type: "varchar",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ColorOfLeaf",
+                table: "Plantas",
+                type: "varchar",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Plantas",
+                type: "varchar",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Desease",
+                table: "Plantas",
+                type: "varchar",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "HeightAtPurchase",
+                table: "Plantas",
+                type: "varchar",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "HeightPotential",
+                table: "Plantas",
+                type: "varchar",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Insect",
+                table: "Plantas",
+                type: "varchar",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LatinName",
+                table: "Plantas",
+                type: "varchar",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LightIdeal",
+                table: "Plantas",
+                type: "varchar",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LightTolered",
+                table: "Plantas",
+                type: "varchar",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Origin",
+                table: "Plantas",
+                type: "varchar",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OtherName",
+                table: "Plantas",
+                type: "varchar",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Perfume",
+                table: "Plantas",
+                type: "varchar",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PotDiameter",
+                table: "Plantas",
+                type: "varchar",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Pruning",
+                table: "Plantas",
+                type: "varchar",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Style",
+                table: "Plantas",
+                type: "varchar",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TemperatureMax",
+                table: "Plantas",
+                type: "varchar",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TemperatureMin",
+                table: "Plantas",
+                type: "varchar",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Url",
+                table: "Plantas",
+                type: "varchar",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Use",
+                table: "Plantas",
+                type: "varchar",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Watering",
+                table: "Plantas",
+                type: "varchar",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "WidthAtPurchase",
+                table: "Plantas",
+                type: "varchar",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
