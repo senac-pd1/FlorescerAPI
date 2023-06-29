6be455a3ea356f76dfa5dd50554505601f388636
Wagner de Paula <wagnersouzadepaula@gmail.com>
2023-06-29 16:33:25 -0300

    FLC-43 <Migration criadas e aplicadas no banco>

---
+++ Migrations/20230629191806_CreateWishListTable.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlorescerAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateWishListTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ScientificName",
+                table: "Plantas",
+                type: "varchar",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Wishlist",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PlantaId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Wishlist", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Wishlist");
+
+            migrationBuilder.DropColumn(
+                name: "ScientificName",
+                table: "Plantas");
+        }
+    }
+}
